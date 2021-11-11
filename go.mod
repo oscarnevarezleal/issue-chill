0da7c0f4403e5e2ca5c07a7727e79e75a6fedba9
Oscar <fu.wire@gmail.com>
2021-11-11 12:12:28 -0600

    chore: drop content input, include owner, repo and issue.

--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module issue-chill
 go 1.17
 
 require (
+	github.com/google/go-github/v40 v40.0.0
 	github.com/grassmudhorses/vader-go v0.0.0-20191126145716-003d5aacdb71
 	github.com/sethvargo/go-githubactions v0.4.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,67 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/google/go-github/v40/github"
 	"github.com/grassmudhorses/vader-go/lexicon"
 	"github.com/grassmudhorses/vader-go/sentitext"
+	"golang.org/x/oauth2"
 )
 import "github.com/sethvargo/go-githubactions"
 
 func main() {
-	content := githubactions.GetInput("content")
-	if content == "" {
-		githubactions.Fatalf("missing input 'content'")
-	}
-	githubactions.AddMask(content)
+	// content := githubactions.GetInput("content")
+	// if content == "" {
+	// 	githubactions.Fatalf("missing input 'content'")
+	// }
+	// githubactions.AddMask(content)
 
-	parsedtext := sentitext.Parse(content, lexicon.DefaultLexicon)
+	owner := githubactions.GetInput("owner")
+	if owner == "" {
+		githubactions.Fatalf("missing input 'owner'")
+	}
+
+	repo := githubactions.GetInput("repo")
+	if repo == "" {
+		githubactions.Fatalf("missing input 'repo'")
+	}
+
+	issueStr := githubactions.GetInput("issue")
+	if issueStr == "" {
+		githubactions.Fatalf("missing input 'issue'")
+	}
+
+	issueId, err := strconv.Atoi(issueStr)
+	if err != nil {
+		return
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	issue, _, err := client.Issues.Get(ctx, owner, repo, issueId)
+
+	if err != nil {
+		githubactions.Fatalf("Not such issue")
+	}
+
+	issueTitle := *issue.Title
+	issueBody := *issue.Body
+
+	fmt.Printf("%s", issueTitle)
+	fmt.Printf("%s", issueBody)
+
+	parsedtext := sentitext.Parse(issueBody, lexicon.DefaultLexicon)
 	sentiment := sentitext.PolarityScore(parsedtext)
 	fmt.Printf("::set-output name=POS::%f\n", sentiment.Positive)
 	fmt.Printf("::set-output name=NEG::%f\n", sentiment.Negative)
